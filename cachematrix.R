9e2374daf17835f38094e696b5dfe105f660dc6b
Emilie Wolf <emilie.h.wolf@gmail.com>
2017-02-06 01:51:54 -0600

    extend indentions

--- cachematrix.R
+++ cachematrix.R
@@ -10,31 +10,47 @@
 ## This function creates get/set functions to assign values to
 ## the matrices in the parent frame so computations aren't lost
 makeCacheMatrix <- function(x = matrix()) {
-  m <- NULL
-  set <- function(y) {
-    x <<- y
-    m <<- NULL
-  }
-  get <- function() x                             
-  setinverse <- function(solve) m <<- solve
-  getinverse <- function() m
-  list(set = set, get = get,
-       setinverse = setinverse,
-       getinverse = getinverse)  
+  
+        m <- NULL
+  
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+         }
+  
+        get <- function() {
+                x 
+        }                             
+  
+        setinverse <- function(solve) { 
+                m <<- solve 
+        } 
+  
+        getinverse <- function(){
+                m
+         }
+  
+        list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)  
 }
 
 
 ## This function finds the inverse matrix, either by solving it
 ## computationally, or by retrieving it from memory
+
 cacheSolve <- function(x, ...) {
-  m <- x$getinverse()
-  if(!is.null(m)) {
-    message("getting cached data")
-    return(m)
-  }
-  data <- x$get()
-  m <- solve(data, ...)
-  x$setinverse(m)
-  m
+        m <- x$getinverse()
+  
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+      
+        data <- x$get()
+  
+        m <- solve(data, ...)
+  
+        x$setinverse(m)
+
+        m
   ## Return a matrix that is the inverse of 'x'
 }
